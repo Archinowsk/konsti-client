d81978954e6de6180c1e6fb6b4d87758a26555ef
Arttu Hanska <arttu.hanska@gmail.com>
2017-07-24 23:52:12 +0300

    Sort games list by name and starting time

--- src/views/all-games/components/AllGamesList.jsx
+++ src/views/all-games/components/AllGamesList.jsx
@@ -2,36 +2,79 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const AllGamesList = props => {
   const { games, t } = props;
+  // const startTimes = [];
 
-  // const sortedGames = [];
-
-  games.sort((a, b) => {
-    const keyA = new Date(a.updated_at);
-    const keyB = new Date(b.updated_at);
+  // Sort games by starting time
+  const sortedByTime = games.sort((a, b) => {
+    // const keyA = new Date(a.date);
+    // const keyB = new Date(b.date);
+    const keyA = moment(a.date) + a.title;
+    const keyB = moment(b.date) + b.title;
     // Compare the 2 dates
     if (keyA < keyB) return -1;
     if (keyA > keyB) return 1;
     return 0;
   });
 
-  console.log(games);
+  /*
+  games.forEach(game => {
+    startTimes.push(game.date);
+  });
 
-  const GamesList = games.map(game =>
-    <li key={game.id}>
-      <Link to={`/games/${game.id}`}>
-        {game.title}
-      </Link>
-    </li>
-  );
+  const sortedTimes = [...new Set(startTimes)].sort();
+  */
+
+  const GamesList = sortedByTime.map((game, index, array) => {
+    const formattedDate = moment.utc(game.date).format('DD.M.YYYY HH:mm');
+
+    if (index === 0) {
+      return (
+        <div key={game.id}>
+          <p className="title">
+            {formattedDate}
+          </p>
+          <p className="games-list">
+            <Link to={`/games/${game.id}`}>
+              {game.title}
+            </Link>
+          </p>
+        </div>
+      );
+    } else if (
+      typeof array[index + 1] !== 'undefined' &&
+      game.date === array[index + 1].date
+    ) {
+      return (
+        <div key={game.id}>
+          <p className="games-list">
+            <Link to={`/games/${game.id}`}>
+              {game.title}
+            </Link>
+          </p>
+        </div>
+      );
+    }
+    return (
+      <div key={game.id}>
+        <p className="title">
+          {formattedDate}
+        </p>
+        <p className="games-list">
+          <Link to={`/games/${game.id}`}>
+            {game.title}
+          </Link>
+        </p>
+      </div>
+    );
+  });
 
   return (
     <div>
-      <ul>
-        {GamesList}
-      </ul>
+      {GamesList}
     </div>
   );
 };
