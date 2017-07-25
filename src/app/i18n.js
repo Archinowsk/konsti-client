1772e819529ea388179fff8a1f8e5c349ea0853c
Arttu Hanska <arttu.hanska@gmail.com>
2017-07-25 17:38:30 +0300

    Wait for translations to load

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -12,17 +12,6 @@ function loadLocales(url, options, callback) {
   } else if (url === 'fi') {
     callback(fi, { status: '200' });
   }
-  /*
-  try {
-    let waitForLocale = require(`bundle-loader!../locales/${url}.json`);
-
-    waitForLocale(locale => {
-      callback(locale, { status: '200' });
-    });
-  } catch (e) {
-    callback(null, { status: '404' });
-  }
-  */
 }
 
 i18n.use(XHR).use(Cache).use(LanguageDetector).init({
@@ -30,21 +19,11 @@ i18n.use(XHR).use(Cache).use(LanguageDetector).init({
     loadPath: '{{lng}}',
     parse: data => data,
     ajax: loadLocales,
-    // Path to POST missing resources
-    // addPath: '/locales/add/{{lng}}/',
   },
-  // Calls save missing key function on backend if key not found
-  // TODO: Save missing translations to server not working
-  // saveMissing: true,
 
-  lng: 'en',
+  // lng: 'en',
   fallbackLng: 'en',
-  wait: false, // globally set to wait for loaded translations in translate hoc
-
-  // Have a common namespace used around the full app
-  // ns: ['common'],
-  // defaultNS: 'common',
-
+  wait: true, // globally set to wait for loaded translations in translate hoc
   // debug: true,
 
   /*
@@ -54,17 +33,6 @@ i18n.use(XHR).use(Cache).use(LanguageDetector).init({
     prefix: 'i18next_',
   },
   */
-
-  /*
-    interpolation: {
-      escapeValue: false, // Not needed for React!
-      formatSeparator: ',',
-      format(value, format, lng) {
-        if (format === 'uppercase') return value.toUpperCase();
-        return value;
-      },
-    },
-    */
 });
 
 export default i18n;
