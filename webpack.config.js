e72928241d61f46b6cec1500d784aa89438f5104
Arttu Hanska <arttu.hanska@gmail.com>
2018-07-25 00:24:40 +0300

    Remove unnecessary configs from CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -157,11 +157,7 @@ const prodConfig = {
   },
 
   plugins: [
-    new CleanWebpackPlugin(['build'], {
-      root: path.resolve(__dirname),
-      verbose: false,
-      exclude: ['.keep'],
-    }),
+    new CleanWebpackPlugin(['build']),
     new CopyWebpackPlugin([{ from: 'assets' }]),
     // “en” is built into Moment and can’t be removed
     new MomentLocalesPlugin({
