27cbef5be189604ecb55bac63c3aa66ee4e4c668
Arttu Hanska <arttu.hanska@gmail.com>
2018-07-26 01:31:18 +0300

    Basic UI for group forming

--- src/views/group/GroupView.js
+++ src/views/group/GroupView.js
@@ -11,19 +11,72 @@ type Props = {
 
 type State = {
   loading: boolean,
+  showAddMember: boolean,
+  showJoinGroup: boolean,
 }
 
 class GroupView extends React.Component<Props, State> {
-  state = { loading: true }
+  state = { loading: true, showAddMember: false, showJoinGroup: false }
 
   componentDidMount = async () => {
     await getData()
     this.setState({ loading: false })
   }
 
+  openGroupForming = () => {
+    this.setState({ showAddMember: true, showJoinGroup: false })
+  }
+
+  openJoinGroup = () => {
+    this.setState({ showJoinGroup: true, showAddMember: false })
+  }
+
+  addMember = () => {
+    console.log('add member')
+  }
+
+  joinGroup = () => {
+    console.log('join group')
+  }
+
   render() {
     const { t } = this.props
-    const { loading } = this.state
+    const { loading, showAddMember, showJoinGroup } = this.state
+
+    const AddMemberInput = () => {
+      const { t } = this.props
+
+      return (
+        <div>
+          <p>{t('addGroupMember')} </p>
+
+          <input
+            className="form-input"
+            // {...input}
+            placeholder={t('enterGroupMemberCode')}
+            // type={type}
+            // id={input.name}
+          />
+        </div>
+      )
+    }
+
+    const JoinGroupInput = () => {
+      const { t } = this.props
+
+      return (
+        <div>
+          <p>{t('joinGroup')} </p>
+          <input
+            className="form-input"
+            // {...input}
+            placeholder={t('enterGroupLeaderCode')}
+            // type={type}
+            // id={input.name}
+          />
+        </div>
+      )
+    }
 
     return (
       <div className="group-view">
@@ -31,8 +84,30 @@ class GroupView extends React.Component<Props, State> {
         {!loading && (
           <React.Fragment>
             <p className="page-title">{t('pages.group')}</p>
-            <button>{t('button.createGroup')}</button>
-            <button>{t('button.joinGroup')}</button>
+            <button onClick={() => this.openGroupForming()}>
+              {t('button.createGroup')}
+            </button>
+            <button onClick={() => this.openJoinGroup()}>
+              {t('button.joinGroup')}
+            </button>
+
+            {showAddMember && (
+              <div>
+                <AddMemberInput />
+                <button onClick={() => this.addMember()}>
+                  {t('button.addMember')}
+                </button>
+              </div>
+            )}
+
+            {showJoinGroup && (
+              <div>
+                <JoinGroupInput />
+                <button onClick={() => this.joinGroup()}>
+                  {t('button.joinGroup')}
+                </button>
+              </div>
+            )}
           </React.Fragment>
         )}
       </div>
