c1447a051bad0591d41061b7ad8daed43c578dc7
Arttu Hanska <arttu.hanska@gmail.com>
2018-07-25 14:36:39 +0300

    Set selected games from myGames

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -3,6 +3,7 @@ import { submitGetResults } from 'views/all-signups/allSignupsActions'
 import { submitGetGames } from 'views/all-games/allGamesActions'
 import { submitGetSettings } from 'views/admin/adminActions'
 import { submitGetUser } from 'views/my-games/myGamesActions'
+import { submitSelectedGames } from 'views/signup/signupActions'
 
 const loadData = async (store: Object) => {
   const state = store.getState()
@@ -11,19 +12,27 @@ const loadData = async (store: Object) => {
   const myGamesLoaded = state.myGames.myGamesLoaded
   const loggedIn = state.login.loggedIn
 
+  // Get games data
   await store.dispatch(submitGetGames())
 
   if (!adminSettingsLoaded) {
+    // Get settings data
     await store.dispatch(submitGetSettings())
   }
 
   if (!myGamesLoaded) {
+    // Get results data
     await store.dispatch(submitGetResults())
   }
 
   if (loggedIn) {
+    // Get user data
     const username = state.login.username
     await store.dispatch(submitGetUser(username))
+
+    // Set selected games
+    const selectedGames = store.getState().myGames.signedGames
+    await store.dispatch(submitSelectedGames(selectedGames))
   }
 }
 
