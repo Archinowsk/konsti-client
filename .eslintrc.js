27950f65c31701e03d03adbb7c7ebb0057084f39
Arttu Hanska <arttu.hanska@gmail.com>
2018-12-30 14:53:07 +0200

    Re-add eslint-config-standard-react rules removed before

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,5 +48,34 @@ module.exports = {
     'prettier/prettier': 'error',
     'compat/compat': 'error',
     // 'flowtype-errors/show-errors': 'error',
+
+    // Rules from eslint-config-standard-react
+    'react/jsx-no-bind': [
+      'error',
+      {
+        allowArrowFunctions: true,
+        allowBind: false,
+        ignoreRefs: true,
+      },
+    ],
+    'react/no-did-update-set-state': 'error',
+    'react/no-unknown-property': 'error',
+    'react/no-unused-prop-types': 'error',
+    'react/prop-types': 'error',
+    'react/react-in-jsx-scope': 'error',
+
+    // Rules from eslint-config-standard-jsx
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react/jsx-boolean-value': 'error',
+    'react/jsx-curly-spacing': ['error', 'never'],
+    'react/jsx-equals-spacing': ['error', 'never'],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/self-closing-comp': 'error',
   },
 }
