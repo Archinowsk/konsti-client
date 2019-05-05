beef9db7bd657bf80ab7038e6e2c8add81a0d3ad
Arttu Hanska <arttu.hanska@gmail.com>
2019-05-05 21:27:31 +0300

    Sort by nested key

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -6,6 +6,10 @@ export const sortArrayByKey = <T: Object>(
 ): Array<T> => {
   if (!array || !key1) return []
 
+  if (key1.includes('.')) {
+    return sortArrayByNestedKey(array, key1)
+  }
+
   if (key2) {
     return array.sort((a, b) => {
       const keyA = a[key1].toLowerCase() + a[key2].toLowerCase()
@@ -19,6 +23,25 @@ export const sortArrayByKey = <T: Object>(
   return array.sort((a, b) => {
     const keyA = a[key1].toLowerCase()
     const keyB = b[key1].toLowerCase()
+    if (keyA < keyB) return -1
+    if (keyA > keyB) return 1
+    return 0
+  })
+}
+
+export const sortArrayByNestedKey = <T: Object>(
+  array: Array<T>,
+  key1: string
+): Array<T> => {
+  if (!array || !key1) return []
+
+  const splitKey = key1.split('.')
+  const firstKey = splitKey[0]
+  const secondKey = splitKey[1]
+
+  return array.sort((a, b) => {
+    const keyA = a[firstKey][secondKey].toLowerCase()
+    const keyB = b[firstKey][secondKey].toLowerCase()
     if (keyA < keyB) return -1
     if (keyA > keyB) return 1
     return 0
