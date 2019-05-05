4b8ddc769fb8d843b3885b920c2e80f27e8a8cbe
Arttu Hanska <arttu.hanska@gmail.com>
2019-05-05 19:32:33 +0300

    Add typings to sort util

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,8 +1,13 @@
-export const sortArrayByKey = (games, key1, key2) => {
-  if (!games) return
+/* @flow */
+export const sortArrayByKey = <T: Object>(
+  array: Array<T>,
+  key1: string,
+  key2?: string
+): Array<T> => {
+  if (!array) return []
 
   if (key2) {
-    return games.sort((a, b) => {
+    return array.sort((a, b) => {
       const keyA = a[key1].toLowerCase() + a[key2].toLowerCase()
       const keyB = b[key1].toLowerCase() + b[key2].toLowerCase()
       if (keyA < keyB) return -1
@@ -11,7 +16,7 @@ export const sortArrayByKey = (games, key1, key2) => {
     })
   }
 
-  return games.sort((a, b) => {
+  return array.sort((a, b) => {
     const keyA = a[key1].toLowerCase()
     const keyB = b[key1].toLowerCase()
     if (keyA < keyB) return -1
