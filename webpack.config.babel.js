762d150f7ecda2e76975c9cac094820b31565fe1
Arttu Hanska <arttu.hanska@gmail.com>
2019-05-07 21:17:30 +0300

    Add separate staging config and use terser

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,7 @@ import ExtractTextWebpackPlugin from 'extract-text-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import TemplateWebpackPlugin from 'html-webpack-template'
 import MomentLocalesPlugin from 'moment-locales-webpack-plugin'
-import UglifyJSPlugin from 'uglifyjs-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 import BrotliPlugin from 'brotli-webpack-plugin'
 // import PrepackWebpackPlugin from 'prepack-webpack-plugin'
 import Dotenv from 'dotenv-webpack'
@@ -14,7 +14,6 @@ import webpack from 'webpack'
 import webpackMerge from 'webpack-merge'
 import appConfig from './src/config'
 
-let config = null
 const TARGET = process.env.npm_lifecycle_event
 
 const stats = {
@@ -177,31 +176,17 @@ const prodConfig = {
       asset: '[path].br[query]',
       test: /\.js$/,
     }),
-  ],
-
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-}
-
-const prodEnv = {
-  plugins: [
     new Dotenv({
-      path: './.prod.env', // Path to .env file (this is the default)
+      path: './.prod.env',
     }),
   ],
 
   optimization: {
     minimizer: [
-      new UglifyJSPlugin({
-        uglifyOptions: {
+      new TerserPlugin({
+        terserOptions: {
           compress: {
-            drop_console: appConfig.dropConsole,
-          },
-          output: {
-            comments: false,
+            drop_console: true,
           },
         },
       }),
@@ -209,20 +194,81 @@ const prodEnv = {
   },
 }
 
-const stagingEnv = {
+const stagingConfig = {
+  mode: 'production',
+
+  stats,
+
+  devtool: 'source-map',
+
+  module: {
+    // Loaders to transform sources
+    rules: [
+      {
+        // SCSS loaders
+        test: /\.scss$/,
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 2 },
+            },
+            { loader: 'postcss-loader' },
+            { loader: 'sass-loader' },
+          ],
+          publicPath: '/',
+        }),
+      },
+    ],
+  },
+
   plugins: [
+    new CleanWebpackPlugin(),
+    // new PrepackWebpackPlugin(),
+    new CopyWebpackPlugin([{ from: 'assets' }]),
+    // “en” is built into Moment and can’t be removed
+    new MomentLocalesPlugin({
+      localesToKeep: ['fi'],
+    }),
+    new ExtractTextWebpackPlugin('[name].bundle.css'),
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$/,
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.js$/,
+    }),
     new Dotenv({
-      path: './.staging.env', // Path to .env file (this is the default)
+      path: './.staging.env',
     }),
   ],
+
+  optimization: {
+    minimize: false,
+    namedModules: true,
+    namedChunks: true,
+    moduleIds: 'named',
+    chunkIds: 'named',
+  },
 }
 
-if (TARGET === 'build' || TARGET === 'analyze') {
-  config = webpackMerge(commonConfig, prodConfig, prodEnv)
-} else if (TARGET === 'build-staging') {
-  config = webpackMerge(commonConfig, prodConfig, stagingEnv)
-} else if (TARGET === 'start' || TARGET === 'watch') {
-  config = webpackMerge(commonConfig, devConfig)
+const getConfig = () => {
+  if (TARGET === 'build' || TARGET === 'analyze') {
+    return webpackMerge(commonConfig, prodConfig)
+  } else if (TARGET === 'build-staging') {
+    return webpackMerge(commonConfig, stagingConfig)
+  } else if (
+    TARGET === 'start' ||
+    TARGET === 'watch' ||
+    TARGET === 'build-dev'
+  ) {
+    return webpackMerge(commonConfig, devConfig)
+  }
 }
+
+const config = getConfig()
 
 export default config
