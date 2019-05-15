da27d5e90ae63580d201d2a6a23796858526f7fe
Arttu Hanska <arttu.hanska@gmail.com>
2019-05-15 22:11:08 +0300

    Disable React strict mode

--- src/index.js
+++ src/index.js
@@ -30,13 +30,13 @@ const render = () => {
   if (!rootElement) return
 
   ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <React.Suspense fallback={Loading}>
-          <App />
-        </React.Suspense>
-      </Provider>
-    </React.StrictMode>,
+    // <React.StrictMode>
+    <Provider store={store}>
+      <React.Suspense fallback={Loading}>
+        <App />
+      </React.Suspense>
+    </Provider>,
+    // </React.StrictMode>,
     rootElement
   )
 }
