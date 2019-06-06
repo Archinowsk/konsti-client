eca13d07c354a908848eea1dfbaa577d951464ce
Arttu Hanska <arttu.hanska@gmail.com>
2019-06-06 21:17:21 +0300

    Remove unused postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,12 @@
 const postcssPresetEnv = require('postcss-preset-env')
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes')
 const autoprefixer = require('autoprefixer')
-const postcssAutoreset = require('postcss-autoreset')
-const postcssNormalize = require('postcss-normalize')
-
-const autoresetConfig = {
-  reset: 'initial',
-  reset: {
-    margin: 0,
-    padding: 0,
-    flexGrow: 0,
-    flexShrink: 1,
-    flexBasis: 'auto',
-  },
-}
 
 module.exports = {
   parser: 'postcss-scss',
   plugins: [
     postcssPresetEnv,
     postcssFlexbugsFixes,
-    // postcssAutoreset(autoresetConfig),
-    // postcssNormalize,
     autoprefixer, // Call autoprefixer last
   ],
 }
