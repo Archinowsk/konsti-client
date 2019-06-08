ec1612191f93c9b5cde5181971ecc76ef229d6be
Arttu Hanska <arttu.hanska@gmail.com>
2019-06-08 16:45:24 +0300

    Remove registrationReducer

--- src/views/registration/registrationActions.js
+++ src/views/registration/registrationActions.js
@@ -2,30 +2,20 @@
 import { postRegistration } from 'services/userServices'
 import { submitLogin } from 'views/login/loginActions'
 
-export const SUBMIT_REGISTRATION = 'SUBMIT_REGISTRATION'
-
-const submitRegistrationAsync = ({ registrationResponse }) => {
-  return {
-    type: SUBMIT_REGISTRATION,
-    payload: registrationResponse,
-  }
-}
-
 export const submitRegistration = (registrationData: Object) => {
   return async (dispatch: Function) => {
     let response = null
     try {
       response = await postRegistration(registrationData)
     } catch (error) {
-      console.log(`postRegistration error:`, error)
-      dispatch(submitRegistrationAsync({ registrationResponse: error }))
+      return Promise.reject(error)
     }
 
     if (response && response.error) {
       return Promise.reject(response)
     }
+
     if (response && response.status === 'success') {
-      dispatch(submitRegistrationAsync({ registrationResponse: true }))
       dispatch(submitLogin(registrationData))
     }
 
