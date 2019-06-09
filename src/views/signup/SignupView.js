22b1ecb9fe2fa9554cf7ca6c1de37e63e3af2318
Arttu Hanska <arttu.hanska@gmail.com>
2019-06-09 15:03:18 +0300

    Remove unnecessary gameDetails route

--- src/views/signup/SignupView.js
+++ src/views/signup/SignupView.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import SignupList from 'views/signup/components/SignupList'
-import GameDetails from 'components/GameDetails'
 import getOpenSignupTimes from 'utils/getOpenSignupTimes'
 import type { Game } from 'flow/game.flow'
 import type { StatelessFunctionalComponent } from 'react'
@@ -33,14 +32,7 @@ const SignupView: StatelessFunctionalComponent<Props> = (props: Props) => {
         <Route
           exact
           path='/signup'
-          render={props => (
-            <SignupList {...props} games={games} signupTimes={signupTimes} />
-          )}
-        />
-        <Route
-          exact
-          path='/games/:id'
-          render={props => <GameDetails {...props} />}
+          render={() => <SignupList games={games} signupTimes={signupTimes} />}
         />
       </Switch>
     </div>
