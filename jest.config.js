fa6301723d2447aaf313ffd2d5b73fe7078676f4
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-13 14:36:42 +0300

    Add Jest fileMock for image files

--- jest.config.js
+++ jest.config.js
@@ -76,7 +76,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(jpg|png|gif|)$': '<rootDir>/src/test/fileMock.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
