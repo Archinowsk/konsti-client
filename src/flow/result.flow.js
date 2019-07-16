4e7183e5d4eb10c419ea4a693c6f189f1c931430
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-16 22:03:14 +0300

    Remove signedGames from results

--- src/flow/result.flow.js
+++ src/flow/result.flow.js
@@ -4,7 +4,6 @@ import type { Signup } from 'flow/user.flow'
 export type Result = {|
   +username: string,
   +enteredGame: Signup,
-  +signedGames: $ReadOnlyArray<Signup>,
 |}
 
 export type Results = {|
