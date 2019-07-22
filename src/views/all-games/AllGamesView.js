378baf5452be0606607365b28d0fae03a94c14e9
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-22 17:11:29 +0300

    Show only upcoming games and add toggle to show all

--- src/views/all-games/AllGamesView.js
+++ src/views/all-games/AllGamesView.js
@@ -2,8 +2,10 @@
 import React, { Fragment } from 'react'
 import { useSelector } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
+import moment from 'moment'
 import GameDetails from 'views/all-games/components/GameDetails'
 import { AllGamesList } from 'views/all-games/components/AllGamesList'
+import { config } from 'config'
 import type { Game } from 'flow/game.flow'
 import type { StatelessFunctionalComponent, Element } from 'react'
 
@@ -12,31 +14,38 @@ type Props = {}
 export const AllGamesView: StatelessFunctionalComponent<Props> = (
   props: Props
 ): Element<typeof Fragment> => {
+  const { useTestTime } = config
+
   const games: $ReadOnlyArray<Game> = useSelector(state => state.allGames.games)
+  const testTime: string = useSelector(state => state.admin.testTime)
   const hiddenGames: $ReadOnlyArray<Game> = useSelector(
     state => state.admin.hiddenGames
   )
 
-  // Remove hidden games
-  const getVisibleGames = () => {
-    if (!hiddenGames) return games
-    const visibleGames = []
-    for (const game of games) {
-      let match = false
-      for (const hiddenGame of hiddenGames) {
-        if (game.gameId === hiddenGame.gameId) {
-          match = true
-          break
-        }
-      }
-      if (!match) {
-        visibleGames.push(game)
-      }
-    }
-    return visibleGames
-  }
+  const [showAllGames, setShowAllGames] = React.useState(false)
+  ;(showAllGames: boolean)
 
-  const visibleGames = getVisibleGames()
+  const getVisibleGames = (): $ReadOnlyArray<Game> => {
+    let timeNow = moment()
+    if (useTestTime) {
+      timeNow = moment(testTime)
+    }
+
+    const visibleGames = games.filter(game => {
+      const hidden = hiddenGames.find(
+        hiddenGame => game.gameId === hiddenGame.gameId
+      )
+      if (!hidden) return game
+    })
+
+    if (showAllGames) return visibleGames
+
+    const upcomingGames = visibleGames.filter(visibleGame =>
+      moment(visibleGame.startTime).isAfter(timeNow)
+    )
+
+    return upcomingGames
+  }
 
   return (
     <Fragment>
@@ -44,7 +53,25 @@ export const AllGamesView: StatelessFunctionalComponent<Props> = (
         <Route
           exact
           path='/games'
-          render={() => <AllGamesList games={visibleGames} />}
+          render={() => (
+            <Fragment>
+              <div className='all-games-toggle-visibiliy'>
+                <button
+                  onClick={() => setShowAllGames(false)}
+                  disabled={!showAllGames}
+                >
+                  Upcoming games
+                </button>
+                <button
+                  onClick={() => setShowAllGames(true)}
+                  disabled={showAllGames}
+                >
+                  All games
+                </button>
+              </div>
+              <AllGamesList games={getVisibleGames()} />
+            </Fragment>
+          )}
         />
         <Route path='/games/:id' render={() => <GameDetails />} />
       </Switch>
