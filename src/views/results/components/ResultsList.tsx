7a31b40b02c55cd6463fbf54258046715a03e368
Altesmi <optikkanen@gmail.com>
2020-02-26 21:17:27 +0200

    Search field added

--- src/views/results/components/ResultsList.tsx
+++ src/views/results/components/ResultsList.tsx
@@ -13,20 +13,37 @@ export const ResultsList: FunctionComponent<Props> = (
 ): ReactElement<'div'> => {
   const { results } = props;
   const { t } = useTranslation();
-
   const [sortedBy, setSortedBy] = React.useState<string>('');
+  const [searchTerm, setSearchTerm] = React.useState<string>('');
+  const [searchResults, setSearchResults] = React.useState<any>([]);
 
   React.useEffect(() => {
     setSortedBy('username');
   }, []);
 
+  React.useEffect(() => {
+    setSearchResults(
+      results.filter(result => {
+        return (
+          result.username.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          result.enteredGame.gameDetails.title
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
+        );
+      })
+    );
+  }, [searchTerm]);
+
   const buttons = ['username', 'gameTitle'];
+
+  const handleSearchFieldChange = e => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div className='results-list'>
       <div className='results-button-row'>
         <span>{t('sortBy')} </span>
-
         {buttons.map(name => {
           return (
             <button
@@ -39,9 +56,28 @@ export const ResultsList: FunctionComponent<Props> = (
             </button>
           );
         })}
+        <br />
+        <span>{t('find')} </span>
+        <input
+          type='text'
+          value={searchTerm}
+          onChange={handleSearchFieldChange}
+        />
       </div>
-      {sortedBy === 'username' && <ResultsByUsername results={results} />}
-      {sortedBy === 'gameTitle' && <ResultsByGameTitle results={results} />}
+      {sortedBy === 'username' && (
+        <ResultsByUsername
+          results={
+            Object.entries(searchResults).length === 0 ? results : searchResults
+          }
+        />
+      )}
+      {sortedBy === 'gameTitle' && (
+        <ResultsByGameTitle
+          results={
+            Object.entries(searchResults).length === 0 ? results : searchResults
+          }
+        />
+      )}
     </div>
   );
 };
